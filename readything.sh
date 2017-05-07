8bfc9b52f13a892f08578fdeace164d7a90d6551
Keenan Verbrugge <keenanv@gmail.com>
2017-05-07 13:03:02 -0700

    add checks for iam resources

--- readything.sh
+++ readything.sh
@@ -28,7 +28,7 @@ if [ -z "$(aws iam get-role --role-name nw-rt-$CLIENT | grep RoleId)" ]; then
           --role-name nw-rt-$CLIENT \
           --assume-role-policy-document file://$OUTPUT/ec2-$CLIENT-trust.json
 else
-  echo "Role nw-rt-$CLIENT exists. Not creating."
+  echo "Role nw-rt-$CLIENT exists. Skipping...."
 fi
 
 # Check if Policy Already Exists
@@ -39,19 +39,37 @@ if [ -z "$(aws iam get-role-policy --role-name nw-rt-$CLIENT --policy-name nw-rt
           --policy-name nw-rt-s3-$CLIENT \
           --policy-document file://$OUTPUT/s3-$CLIENT.json
 else
-  echo "Policy nw-rt-s3-$CLIENT exists. Not creating."
+  echo "Policy nw-rt-s3-$CLIENT exists. Skipping..."
 fi
 
-aws iam attach-role-policy \
-        --policy-arn arn:aws:iam::729480203074:policy/nw-rt-s3-users \
-        --role-name nw-rt-$CLIENT
+# Check if policy is attached already
+if [ -z "$(aws iam list-attached-role-policies --role-name nw-rt-$CLIENT | grep nw-rt-s3-users)" ]; then
+  # Todo: Create policy if doesn't exist
+  echo "Policy is not attached to role. Attaching."
+  aws iam attach-role-policy \
+          --policy-arn arn:aws:iam::729480203074:policy/nw-rt-s3-users \
+          --role-name nw-rt-$CLIENT
+else
+  echo "Policy is already attached. Skipping..."
+fi
 
-aws iam create-instance-profile \
-        --instance-profile-name nw-rt-ip-$CLIENT
+# Check if Instance Profile Already Exists
+if [ -z "$(aws iam get-instance-profile --instance-profile-name nw-rt-ip-$CLIENT)" ]; then
+  echo "Instance profile does not exist. Creating."
+  aws iam create-instance-profile \
+          --instance-profile-name nw-rt-ip-$CLIENT
+else
+  echo "Instance profile already exists. Skipping..."
+fi
 
-aws iam add-role-to-instance-profile \
-        --instance-profile-name nw-rt-ip-$CLIENT \
-        --role-name nw-rt-$CLIENT
+if [ -z "$(aws iam list-instance-profiles-for-role --role-name nw-rt-$CLIENT|grep nw-rt-ip-$CLIENT)" ]; then
+  echo "Role is not attached to instance profile. Attaching."
+  aws iam add-role-to-instance-profile \
+          --instance-profile-name nw-rt-ip-$CLIENT \
+          --role-name nw-rt-$CLIENT
+else
+  echo "Role is already attached to instance profile. Skipping..."
+fi
 
 # have to give AWS a few seconds for policies/S3
 echo "sleeping..."
