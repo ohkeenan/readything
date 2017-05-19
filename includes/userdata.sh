714183ebeb8a8681750b0b38ce022521e7eea9b9
Keenan Verbrugge <keenanv@gmail.com>
2017-05-19 11:33:02 -0700

    add config file

--- includes/userdata.sh
+++ includes/userdata.sh
@@ -56,10 +56,6 @@ useradd -r s3fs -u 141 -g 141
 echo "$BUCKET:/users/$CLIENTSEC/  /mnt/s3/  fuse.s3fs    _netdev,allow_other,iam_role=$BUCKET-$CLIENT,uid=141,gid=141,umask=007,use_cache=/tmp   0   0" >> /etc/fstab
 mount -a
 
-# Chef Stuffs #2
-
-cp /mnt/s3/
-
 # Bring PHP to version 7 and install modules for NextCloud
 yum install -y php70-mysqlnd php70-fpm php70-ctype php70-dom php70-gd php70-mbstring php70-pdo php70-iconv php70-json php70-libxml php70-posix php70-zip php70-zlib php70-curl php70-bz2 php70-mcrypt php70-openssl php70-intl php70-fileinfo php70-exif php70-xml php70-imagick php70-json
 pkill php-fpm
